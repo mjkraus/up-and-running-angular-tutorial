1395fba459bc710b8f4e698ca5a4d62c56e5b3b6
Molly Kraus <mollyjkraus@gmail.com>
2016-06-30 22:16:53 -0400

    add myApp module to controller

---
+++ js/controllers.js
@@ -0,0 +1,5 @@
+// this is the myApp module
+// dependencies go in [], but don't need them for this application
+// myApp has all the code for the application - this is an EX of namespacing. 
+//Namespacing protects the app code, so no other script interfers with our application code
+var myApp = angular.module('myApp', []);
